495c994ed602e0469f64dba1681fe3bf89006722
cyber-mat <olaniyanolamide@gmail.com>
2020-07-28 11:55:57 +0100

    Buit and styled the collection preview component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './shop.styles.scss';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview id={id} key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
