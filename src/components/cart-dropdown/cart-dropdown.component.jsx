d922b6d6c19254e9686b2e5b40a80b4c7a2be5fb
cyber-mat <olaniyanolamide@gmail.com>
2020-08-03 07:43:38 +0100

    Cart items now being updated on UI

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-item'></div>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton to='/checkout'>Go to checkout</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+// Check the root-reducer file to understand the argument destructuring below
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
