d92c0573c6d7791833e719d0b1815413f2e1c03b
cyber-mat <olaniyanolamide@gmail.com>
2020-08-03 08:54:57 +0100

    Updating the cart icon number of items dynamically. Refactored code to use reselect library for memoization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import './cart-dropdown.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
 );
 
 // Check the root-reducer file to understand the argument destructuring below
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
